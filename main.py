8dfd567d710bcde8d1219412941ec744d1a9ffa3
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2024-07-26 20:50:15 +0300

    Add GPRMC string sending to serial port

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import serial
 from serial.tools import list_ports
 import threading
+import time
+import NMEA0183
 
 comPort = "COM3"
 try:
@@ -21,13 +23,34 @@ except serial.SerialException:
 stop_communication = False
 
 def send_user_input_to_arduino():
+    nmea = NMEA0183.NMEA()
+
+    coords = [
+        [51.22521374836571, 33.19309502652848],
+        [51.22534517983234, 33.193290961423074],
+        [51.2257806501225, 33.19382045193892],
+        [51.22582859155405, 33.19387148716936],
+        [51.2259124889391, 33.19396717822644],
+        [51.22600437637579, 33.194082007494934],
+        [51.22614020964186, 33.194215974974846],
+        [51.22630001298017, 33.19440097771017],
+        [51.22652373670881, 33.1946880508814],
+        [51.22667954366612, 33.19491133002455]
+    ]
+
     global stop_communication
-    while True:
-        data = input("Enter data to send: ")
-        if data == '$quit()':
-            stop_communication = True
-            break
-        ser.write(data.encode())  # Send data to Arduino
+    for i in range(0, 16):
+        index = i
+        if i >= len(coords):
+            index = i % len(coords)
+            
+        time.sleep(1)
+        message = nmea.generate_gprmc('102340.000', coords[index][0], coords[index][1], 10.2, 23.5, '200724', 13.4)
+        print(message)
+        ser.write(message.encode())  # Send data to Arduino
+
+    time.sleep(2)
+    stop_communication = True
 
 def receive_from_ser():
     while True:
